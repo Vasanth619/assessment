0f6c4660de809a3ee1857ec2b56202598f8d69f2
Vasanth619 <vasanth.clorida@gmail.com>
2018-11-21 19:11:13 +0530

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,35 +6,41 @@
 <section class="card">
     <div class="card-header">
         <span class="utils__title">
-            <strong>Editable Tables</strong>
-            <a href="http://mindmup.github.io/editable-table/" target="_blank" class="btn btn-sm btn-primary ml-2">Official Documentation <i class="icmn-link ml-1"><!-- --></i></a>
+
+            <strong>Total Questions</strong>
+                    <select class="selectpicker">
+                        <option>Select Category</option>
+                        <option value="">Drupal</option>
+                        <option value="">PHP</option>
+                        <option value="">Java</option>
+                    </select>
         </span>
     </div>
     <div class="card-body">
         <div class="row">
             <div class="col-lg-12">
-                <h5 class="text-black"><strong>Basic Editable Table</strong></h5>
-                <p class="text-muted">Element: read <a href="http://mindmup.github.io/editable-table/" target="_blank">official documentation<small><i class="icmn-link ml-1"><!-- --></i></small></a></p>
+                <!-- <h5 class="text-black"><strong>Basic Editable Table</strong></h5>
+                <p class="text-muted">Element: read <a href="http://mindmup.github.io/editable-table/" target="_blank">official documentation<small><i class="icmn-link ml-1"></i></small></a></p> -->
                 <div class="table-responsive mb-5">
                     <table class="table table-hover nowrap editable-table" id="example1" style="cursor: pointer;">
                         <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Date</th>
-                            <th>Salary</th>
+                            <th>#</th>
+                            <th>Question</th>
+                            <th>Created Date</th>
+                            <th>Updated Date</th>
+                            <th>Status</th>
+                            <th>Action</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
-                            <td tabindex="1">Damon</td>
+                            <td tabindex="1">1</td>
                             <td tabindex="1">5516 Adolfo Green</td>
                             <td tabindex="1">Littelhaven</td>
                             <td tabindex="1">85</td>
-                            <td tabindex="1">2014/06/13</td>
-                            <td tabindex="1">$553,536</td>
+                            <td tabindex="1">Inactive</td>
+                            <td tabindex="1">Edit / Delete</td>
                         </tr>
                         </tbody>
                         
@@ -46,6 +52,19 @@
     </div>
 </section>
 
+
+<script>
+  (function($) {
+    "use strict";
+    $(function () {
+
+      $('#example1').DataTable({
+        responsive: true
+      });
+
+    });
+  })(jQuery)
+</script>
 @endsection
 
 
